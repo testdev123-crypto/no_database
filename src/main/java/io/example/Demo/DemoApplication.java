46bebf017dccc19e932f22fc7ada8034daa1eb73
Rohan <rohanpai126@gmail.com>
2024-08-27 13:12:55 +0530

    1st commit

---
+++ src/main/java/io/example/Demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package io.example.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("hi");
+		System.out.println("hello");
+		for(int i=0;i<5;i++){
+			System.out.println("testing");
+		}
+		for(int i=0;i<5;i++){
+			System.out.println("test");
+		}
+		System.out.println("Hi");
+		System.out.println("testing");
+	}
+
+}
